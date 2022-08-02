3352a43a1a1f62102851ff3414a99093c7aa01a8
P1tirim <andrey.orishev@mail.ru>
2022-08-02 16:06:17 +0300

    v0.0102
    Add jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour
     public FixedJoystick fixedJoystick;
     Rigidbody2D rb;
     public float speed;
+    public float jumpForce;
+    bool canJump = true;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         
+      
     }
 
     //Move player in horizontal direction
@@ -25,7 +28,24 @@ public class Player : MonoBehaviour
         targetVelocity = targetVelocity * speed;
         Vector2 velocity = rb.velocity;
         Vector2 velocityChange = (targetVelocity - velocity);
+        velocityChange.y = 0;
         rb.AddForce(velocityChange, ForceMode2D.Impulse);
-        
+
+        if (canJump && fixedJoystick.Vertical > 0.6f)
+        {
+            Debug.Log("jump");
+            canJump = false;
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground") canJump = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground") canJump = false;
     }
 }
